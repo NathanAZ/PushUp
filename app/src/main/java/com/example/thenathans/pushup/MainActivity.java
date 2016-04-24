bfffbb48a97780569caa419671cdbaaa820971b0
Darnell Nathan <Darnellppr@gmail.com>
2016-04-23 18:14:56 -0700

    Initial commit

---
+++ app/src/main/java/com/example/thenathans/pushup/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.thenathans.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void TestUpload(){
+
+        int i = 1 + 1;
+
+        int ii = 2 + 2;
+
+        int iii = i + ii;
+        
+
+    }
+}
