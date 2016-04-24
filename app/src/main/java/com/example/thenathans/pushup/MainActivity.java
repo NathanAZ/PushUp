02e8326e8030ffaa5aee9cfc3627181a43819d7b
Darnell Nathan <Darnellppr@gmail.com>
2016-04-23 18:20:08 -0700

    Add new Class "TestAddFile.java"

--- app/src/main/java/com/example/thenathans/pushup/MainActivity.java
+++ app/src/main/java/com/example/thenathans/pushup/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         int ii = 2 + 2;
 
         int iii = i + ii;
-        
+
 
     }
 }
---
+++ app/src/main/java/com/example/thenathans/pushup/TestAddFile.java
@@ -0,0 +1,7 @@
+package com.example.thenathans.pushup;
+
+/**
+ * Created by The Nathan's on 4/23/2016.
+ */
+public class TestAddFile {
+}
